c7599d3d5ce265c8dbbbb92f04a64f8052f2bbfc
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-08-29 01:31:25 +0900

    :sparkles: Update 2023-2 study contents

--- js/about.js
+++ js/about.js
@@ -11,66 +11,77 @@ const overlay = document.querySelector(".overlay");
 
 // When the user clicks the button, open the modal
 btn[0].onclick = function () {
-  modal[0].style.display = "block";
-  overlay.classList.remove("modal-hidden");
+    modal[0].style.display = "block";
+    overlay.classList.remove("modal-hidden");
 };
 
 btn[1].onclick = function () {
-  modal[1].style.display = "block";
-  overlay.classList.remove("modal-hidden");
+    modal[1].style.display = "block";
+    overlay.classList.remove("modal-hidden");
 };
 
 btn[2].onclick = function () {
-  modal[2].style.display = "block";
-  overlay.classList.remove("modal-hidden");
+    modal[2].style.display = "block";
+    overlay.classList.remove("modal-hidden");
 };
 
 btn[3].onclick = function () {
-  modal[3].style.display = "block";
-  overlay.classList.remove("modal-hidden");
+    modal[3].style.display = "block";
+    overlay.classList.remove("modal-hidden");
 };
 btn[4].onclick = function () {
-  modal[4].style.display = "block";
-  overlay.classList.remove("modal-hidden");
+    modal[4].style.display = "block";
+    overlay.classList.remove("modal-hidden");
+};
+
+btn[5].onclick = function () {
+    modal[5].style.display = "block";
+    overlay.classList.remove("modal-hidden");
 };
 
 // When the user clicks on <closeModal> (x), close the modal
 closeModal[0].onclick = function () {
-  modal[0].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[0].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 
 closeModal[1].onclick = function () {
-  modal[1].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[1].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 
 closeModal[2].onclick = function () {
-  modal[2].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[2].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 
 closeModal[3].onclick = function () {
-  modal[3].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[3].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 closeModal[4].onclick = function () {
-  modal[4].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[4].style.display = "none";
+    overlay.classList.add("modal-hidden");
+};
+
+closeModal[5].onclick = function () {
+    modal[5].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 
 overlay.onclick = function () {
-  modal[0].style.display = "none";
-  modal[1].style.display = "none";
-  modal[2].style.display = "none";
-  modal[3].style.display = "none";
-  modal[4].style.display = "none";
-  overlay.classList.add("modal-hidden");
+    modal[0].style.display = "none";
+    modal[1].style.display = "none";
+    modal[2].style.display = "none";
+    modal[3].style.display = "none";
+    modal[4].style.display = "none";
+    modal[5].style.display = "none";
+    overlay.classList.add("modal-hidden");
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 };
